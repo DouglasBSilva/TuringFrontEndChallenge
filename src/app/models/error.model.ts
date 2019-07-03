a553945dfcdec22c27dc5f9f18c6f78664bf2b02
Dogas <douglasbezerra27@hotmail.com>
2019-07-02 21:33:41 -0300

    Created Error Model

---
+++ src/app/models/error.model.ts
@@ -0,0 +1,21 @@
+export class CartWithProduct {
+    public code: string;
+    public message: string;
+    public field: string;
+    public status: string;
+
+    constructor(
+        code: string,
+        message: string,
+        field: string,
+        status: string
+    ) {
+    this.code = code;
+    this.message = message;
+    this.field = field;
+    this.status = status;
+    }
+
+
+
+}
